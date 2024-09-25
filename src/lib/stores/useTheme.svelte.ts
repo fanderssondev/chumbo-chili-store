703af9d5a2486b57673d0ab87f36ac9abb895648
Fredrik <fredrikanderssondev@gmail.com>
2024-09-25 23:56:17 +0200

    imported theme store

---
+++ src/lib/stores/useTheme.svelte.ts
@@ -0,0 +1,66 @@
+import { browser } from '$app/environment';
+
+function getInitialTheme(): boolean {
+	// Check for a saved theme in local storage
+	let savedTheme: string | null = null;
+
+	if (browser) {
+		savedTheme = localStorage.getItem('darkMode');
+
+		if (savedTheme) {
+			return savedTheme === 'true';
+		} else {
+			// If no saved theme, check the system preference
+			const prefersDark =
+				window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
+			return prefersDark;
+		}
+	}
+	return false;
+}
+
+export function useTheme() {
+	let darkMode = $state<boolean>(getInitialTheme());
+
+	// Check system preference changes
+	if (browser) {
+		window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', (event) => {
+			darkMode = event.matches;
+
+			if (darkMode) {
+				localStorage.setItem('darkMode', 'true');
+			} else {
+				localStorage.setItem('darkMode', 'false');
+			}
+		});
+	}
+
+	// Check user preference changes
+	if (browser) {
+		if (darkMode) {
+			localStorage.setItem('darkMode', 'true');
+		} else {
+			localStorage.setItem('darkMode', 'false');
+		}
+	}
+
+	function toggleDarkmode() {
+		darkMode = !darkMode;
+
+		if (browser) {
+			if (darkMode) {
+				localStorage.setItem('darkMode', 'true');
+			} else {
+				// document.body.dataset.theme = '';
+				localStorage.setItem('darkMode', 'false');
+			}
+		}
+	}
+
+	return {
+		get darkMode() {
+			return darkMode;
+		},
+		toggleDarkmode
+	};
+}
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,55 @@
-<script>
+<script lang="ts">
 	import '../app.css';
+	import Sun from 'lucide-svelte/icons/sun';
+	import Moon from 'lucide-svelte/icons/moon';
+	import { Button } from '$lib/components/ui/button';
+	import { useTheme } from '$lib/stores/useTheme.svelte';
+
+	const { children } = $props();
+
+	let theme = useTheme();
+
+	$effect(() => {
+		theme.darkMode
+			? document.documentElement.classList.add('dark')
+			: document.documentElement.classList.remove('dark');
+	});
 </script>
 
-<slot></slot>
+<Button
+	on:click={theme.toggleDarkmode}
+	variant="outline"
+	size="icon"
+	class="absolute right-5 top-3 rounded-full border-none bg-slate-600 hover:bg-slate-500"
+>
+	<Sun
+		class="h-[1.5rem] w-[1.5rem] rotate-0 scale-0 transition-all dark:-rotate-90 dark:scale-100"
+	/>
+	<Moon
+		class="dark:rotate-270 absolute h-[1.5rem] w-[1.5rem] -rotate-90 scale-100 transition-all dark:scale-0"
+	/>
+	<span class="sr-only">Toggle theme</span>
+</Button>
+
+<header class="max-w-full bg-slate-600 py-3 dark:bg-slate-800">
+	<div class="lg:grid-cols-header container grid items-center">
+		<img src="/pinata.svg" alt="Pinyata logo" class="mr-auto size-10" />
+		<h1
+			class="font-montserrat mr-auto hidden text-4xl font-semibold tracking-wider lg:inline-block"
+		>
+			Pinyata Store
+		</h1>
+		<nav class="col-span-2 col-start-2 inline-block lg:col-start-3">
+			<ul class="flex items-center justify-end gap-4">
+				<li><a href="/products">Products</a></li>
+				<li><a href="/about">About Us</a></li>
+				<li><a href="/contact">Contact Us</a></li>
+				<li><a href="/github.com">Github</a></li>
+			</ul>
+		</nav>
+	</div>
+</header>
+
+<main class="container min-h-screen">
+	{@render children()}
+</main>
