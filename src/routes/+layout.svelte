f17483762c3f933beeeed4c67b802a4801e40b2d
Fredrik <fredrikanderssondev@gmail.com>
2024-09-29 00:23:04 +0200

    Implemented footer

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import '../app.css';
+	import 'iconify-icon';
 	import Sun from 'lucide-svelte/icons/sun';
 	import Moon from 'lucide-svelte/icons/moon';
 	import { Button } from '$lib/components/ui/button';
@@ -16,21 +17,21 @@
 	});
 </script>
 
-<!-- BUG Container not aligned -->
+{#snippet navLinks()}
+	<li><a href="/products">Products</a></li>
+	<li><a href="/about">About Us</a></li>
+	<li><a href="/contact">Contact Us</a></li>
+	<li><a href="/github.com">Github</a></li>
+{/snippet}
+
+<!-- BUG Not responsive -->
 <header class="relative flex max-w-full border-b-2 bg-accent text-secondary-foreground">
 	<div class="container grid items-center">
 		<!-- <img src="/pinata.svg" alt="Pinyata logo" class="mr-auto size-10" /> -->
-		<h1
-			class="font-montserrat mr-auto hidden text-4xl font-semibold tracking-wider lg:inline-block"
-		>
-			Chumbo
-		</h1>
+		<h1 class="font-montserrat mr-auto text-4xl font-semibold tracking-wider">Chumbo</h1>
 		<nav class="col-span-2 col-start-2 inline-block lg:col-start-3">
 			<ul class="flex items-center justify-end gap-4">
-				<li><a href="/products">Products</a></li>
-				<li><a href="/about">About Us</a></li>
-				<li><a href="/contact">Contact Us</a></li>
-				<li><a href="/github.com">Github</a></li>
+				{@render navLinks()}
 			</ul>
 		</nav>
 	</div>
@@ -52,7 +53,46 @@
 	{@render children()}
 </main>
 
-<!-- TODO Implement footer -->
-<footer class="mt-16 flex max-w-full border-b-2 bg-accent text-secondary-foreground">
-	gojdgnoj
+<!-- BUG Not responsive -->
+<footer class="mt-16 border-t-2 bg-accent pt-6 text-secondary-foreground">
+	<div class="container flex justify-center gap-36">
+		<div>
+			<h4 class="text-lg font-semibold text-muted-foreground">Links</h4>
+			<nav class="mb-6">
+				<ul>
+					{@render navLinks()}
+				</ul>
+			</nav>
+		</div>
+		<div class="flex flex-col">
+			<p class="self-center">Contact links</p>
+			<hr class="mb-4 h-px w-40 self-center border-0 bg-primary" />
+			<div class="flex gap-12 self-center">
+				<a
+					href="https://github.com/fanderssondev/chumbo-chili-store"
+					aria-label="Github"
+					target="_blank"><iconify-icon icon="lucide:github" class="text-4xl"></iconify-icon></a
+				>
+				<a
+					href="https://www.linkedin.com/in/fredrikanderssondev/"
+					aria-label="Linkedin"
+					target="_blank"><iconify-icon icon="lucide:linkedin" class="text-4xl"></iconify-icon></a
+				>
+			</div>
+			<p class="mt-auto hidden md:inline-block">
+				&copy; {new Date().getFullYear()}
+				<a href="https://github.com/fanderssondev">Fredrik Andersson</a> | All Rights Reserved
+			</p>
+		</div>
+	</div>
+	<p class="text-center md:hidden">
+		&copy; {new Date().getFullYear()}
+		<a href="https://github.com/fanderssondev">Fredrik Andersson</a> | All Rights Reserved
+	</p>
 </footer>
+
+<style>
+	iconify-icon {
+		display: inline-block;
+	}
+</style>
