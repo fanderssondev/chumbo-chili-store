a7df1e0df16902eb438912da3b294b14320322e4
Fredrik <fredrikanderssondev@gmail.com>
2024-10-10 20:38:51 +0200

    fixed footer responsiveness

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -25,7 +25,7 @@
 <header class="border-b-2 bg-accent text-secondary-foreground">
 	<div class="container flex items-center justify-between px-0 pl-4 sm:px-4">
 		<div class="flex items-center gap-2">
-			<img class="size-8 md:size-12" src="/logo.svg" alt="" />
+			<img class="size-8 md:size-12" src="/chumbo-logo.svg" alt="logo" />
 			<h1 class="font-dosis text-2xl font-bold tracking-wide text-orange-500 md:text-5xl">
 				Chumbo
 			</h1>
@@ -43,8 +43,7 @@
 	{@render children()}
 </main>
 
-<!-- BUG not responsive -->
-<!-- <footer class="border-t-2 bg-accent pt-6">
+<footer class="border-t-2 bg-accent pt-6">
 	<div class="flex justify-center gap-16 px-4 md:container md:gap-36">
 		<div>
 			<h4 class="text-lg font-semibold text-muted-foreground">Links</h4>
@@ -61,12 +60,42 @@
 				<a
 					href="https://github.com/fanderssondev/chumbo-chili-store"
 					aria-label="Github"
-					target="_blank"><iconify-icon icon="lucide:github" class="text-4xl"></iconify-icon></a
+					target="_blank"
+					><svg
+						xmlns="http://www.w3.org/2000/svg"
+						width="24"
+						height="24"
+						viewBox="0 0 24 24"
+						fill="none"
+						stroke="currentColor"
+						stroke-width="1"
+						stroke-linecap="round"
+						stroke-linejoin="round"
+						class="lucide lucide-github"
+						><path
+							d="M15 22v-4a4.8 4.8 0 0 0-1-3.5c3 0 6-2 6-5.5.08-1.25-.27-2.48-1-3.5.28-1.15.28-2.35 0-3.5 0 0-1 0-3 1.5-2.64-.5-5.36-.5-8 0C6 2 5 2 5 2c-.3 1.15-.3 2.35 0 3.5A5.403 5.403 0 0 0 4 9c0 3.5 3 5.5 6 5.5-.39.49-.68 1.05-.85 1.65-.17.6-.22 1.23-.15 1.85v4"
+						/><path d="M9 18c-4.51 2-5-2-7-2" /></svg
+					></a
 				>
 				<a
 					href="https://www.linkedin.com/in/fredrikanderssondev/"
 					aria-label="Linkedin"
-					target="_blank"><iconify-icon icon="lucide:linkedin" class="text-4xl"></iconify-icon></a
+					target="_blank"
+					><svg
+						xmlns="http://www.w3.org/2000/svg"
+						width="24"
+						height="24"
+						viewBox="0 0 24 24"
+						fill="none"
+						stroke="currentColor"
+						stroke-width="1"
+						stroke-linecap="round"
+						stroke-linejoin="round"
+						class="lucide lucide-linkedin"
+						><path
+							d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z"
+						/><rect width="4" height="12" x="2" y="9" /><circle cx="4" cy="4" r="2" /></svg
+					></a
 				>
 			</div>
 			<p class="mt-auto hidden md:inline-block">
@@ -79,7 +108,7 @@
 		&copy; {new Date().getFullYear()}
 		<a href="https://github.com/fanderssondev">Fredrik Andersson</a> | All Rights Reserved
 	</p>
-</footer> -->
+</footer>
 
 {#snippet navLinks()}
 	<li><a class="hover:underline" href="/products">Products</a></li>
@@ -154,3 +183,6 @@
 		</Sheet.Content>
 	</Sheet.Root>
 {/snippet}
+
+<style>
+</style>
